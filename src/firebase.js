f21bf115fbf7e2c872afe8789205259478663194
ramdev222 <ramdevcalope1995@gmail.com>
2020-11-10 11:56:30 +0800

    fourth commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBucH7Kzm8Ds5CbuUs8uQJlzTPh3eZcTzA",
-  authDomain: "instagram-clone-23884.firebaseapp.com",
-  databaseURL: "https://instagram-clone-23884.firebaseio.com",
-  projectId: "instagram-clone-23884",
-  storageBucket: "instagram-clone-23884.appspot.com",
-  messagingSenderId: "671034896143",
-  appId: "1:671034896143:web:3aceafdf2319c9f1fc587a",
+  
+  apiKey: "AIzaSyC2BoZ6rCxqmOFFXaOJoiqWt_5PFLbnzdg",
+  authDomain: "findtagay.firebaseapp.com",
+  databaseURL: "https://findtagay.firebaseio.com",
+  projectId: "findtagay",
+  storageBucket: "findtagay.appspot.com",
+  messagingSenderId: "207452849670",
+  appId: "1:207452849670:web:07e8ecdb4529870e9da587",
+  measurementId: "G-CE4X9HZEK7"
 });
 
 const db = firebaseApp.firestore();
